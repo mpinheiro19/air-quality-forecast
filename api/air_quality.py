8a12f80d18a21f859e994e474e7639391adf7ae2
Marco <mpinheiro19@gmail.com>
2024-02-06 00:36:55 -0300

    refactor: api connector and dump file

--- api/air_quality.py
+++ api/air_quality.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os, logging, requests
+import os, logging, requests, json
 
 # Configure logging
 logging.basicConfig(
@@ -13,6 +13,7 @@ logging.basicConfig(
 
 load_dotenv()
 
+
 class AirQualityApi():
 
 
@@ -22,12 +23,36 @@ class AirQualityApi():
         logging.info(f"Conector de API válido para a cidade {self.city}")
         logging.info(f"Buscando conexão para o token com final {self.api_token[-5:]}")
 
-    def get_available_stations(self):
+    def get_response(self) -> json:
+        response = requests.get(f"https://api.waqi.info/search/?keyword={self.city}&token={self.api_token}")
+        logging.info(f"Api retornando {response.status_code}: {response.json()['status']}")
+        return response.json()
 
-        response=requests.get(f"https://api.waqi.info/search/?keyword={self.city}&token={self.api_token}")
-        logging.info(f"Api retornando status: {response.status_code} \n\n {response.text}")
-        return response.text
+    def response_dump(self, api_response: json, filename='aqi_dump') -> None:
+        with open(f"./artifacts/{filename}.json",'w',encoding='utf8') as fp:
+            json.dump(
+                api_response,
+                fp,
+                indent=4,
+                ensure_ascii=False
+            )
+        logging.info(f"Arquivo salvo em './artifacts/{filename}.json'")
+        return
 
+    def get_available_stations(self, api_response) -> list:
+        try:
+            assert len(api_response['data']) > 0
 
-api=AirQualityApi(city='São Paulo')
-api.get_available_stations()
+            stations_list=[
+                api_response['data'][station_value]['station']['url']
+                for station_value 
+                in range(len(api_response['data']))
+            ]
+
+            logging.info(f"Foram encontradas {len(api_response['data'])} estações para a cidade selecionada!")
+            
+            return stations_list
+
+        except AssertionError as e:
+            logging.warn(f"Não foi possível encontrar estações para o keyword informado: {e}")
+            pass
